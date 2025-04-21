6ba572bfeb22ec3908d50e01db1f462782e4ce96
adir1 <aa7381@bs.amalnet.k12.il>
2025-04-21 21:42:13 +0300

    Initial commit

---
+++ app/Worker.java
@@ -0,0 +1,12 @@
+public class Worker
+{
+    public static final String TABLE_WORKER= "Worker";
+    public static final String CARD_NUMBER= "Card";
+    public static final String KEY_ID= "_id";
+    public static final String NAME="Name";
+    public static final String LAST_NAME="LastName";
+    public static final String PHONE_NUMBER="Phone";
+    public static final String ID="Id";
+    public static final String THE_COMPANY_HE_WORKS_FOR="Company Works";
+
+}
